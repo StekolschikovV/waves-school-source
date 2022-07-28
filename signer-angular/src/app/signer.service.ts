d0590c27c26cc24e0bb363983f850c9443a80246
mk <v.a.stekolschikov@gmail.com>
2022-07-28 16:43:15 +0300

    added angular

--- signer-angular/src/app/signer.service.ts
+++ signer-angular/src/app/signer.service.ts
@@ -1,15 +1,20 @@
 import {Injectable} from '@angular/core';
-import {Signer, UserData} from '@waves/signer';
-import {BehaviorSubject, filter, map, Subject, switchMap, tap} from "rxjs";
+import {Signer} from '@waves/signer';
+import {BehaviorSubject, catchError, filter, of, Subject, switchMap, tap} from "rxjs";
 import {ProviderKeeper} from "@waves/provider-keeper";
+import ProviderLedger from "@waves/provider-ledger";
+import ProviderMetamask from "@waves/provider-metamask";
+import {ProviderCloud} from "@waves.exchange/provider-cloud";
+import {ProviderWeb} from "@waves.exchange/provider-web";
+import ProviderSeed from "@waves/provider-seed";
 
 export enum EAuthProvider {
-  ProviderKeeper,
-  ProviderCloud,
-  ProviderWeb,
-  ProviderLedger,
-  ProviderMetamask,
-  ProviderSeed
+  keeper,
+  cloud,
+  web,
+  ledger,
+  metamask,
+  seed
 }
 
 export interface IUser {
@@ -20,10 +25,13 @@ export interface IUser {
 export interface ILoginEvent {
   type: 'login',
   provider: EAuthProvider
+  seed: string | null
+  timestamp: number
 }
 
 export interface ILogoutEvent {
   type: 'logout'
+  timestamp: number
 }
 
 @Injectable({
@@ -32,13 +40,17 @@ export interface ILogoutEvent {
 export class SignerService {
 
   signer: Signer;
-  event$ = new Subject<ILoginEvent | ILogoutEvent>();
+  event$ = new BehaviorSubject<ILoginEvent | ILogoutEvent | null>(null);
 
   user$ = new BehaviorSubject<IUser | null>(null);
 
-
-  userSubject$ = this.event$
+  eventSubscription2$ = this.event$.subscribe(event => {
+    console.log("+++ !!!3")
+  })
+  eventSubscription$ = this.event$
     .pipe(
+      tap(event => console.log('+++ tap event', event)),
+      // @ts-ignore
       tap(() => {
         const user = this.user$.getValue()
         if (user) {
@@ -47,45 +59,47 @@ export class SignerService {
         }
       }),
       // @ts-ignore
-      filter((event: ILoginEvent | ILogoutEvent) => event.type === 'login'),
+      filter((event: ILoginEvent | ILogoutEvent) => event?.type === 'login'),
       switchMap((event: ILoginEvent) => {
-        console.log("+++event", event)
-
-        let result: Promise<UserData> | null = null
+        console.log("+++")
         const providerType = event.provider
-        // switch (providerType) {
-        //   case EAuthProvider.ProviderKeeper:
-            const keeper = new ProviderKeeper();
-            this.signer.setProvider(keeper);
-            result = this.signer.login()
-        //     break
-        // }
-        return result
-
-        // switch (authType) {
-        //   case EAuthType.ProviderKeeper:
-        //     const keeper = new ProviderKeeper();
-        //     this.signer.setProvider(keeper);
-        //     const loginResult = await this.signer.login()
-        //     console.log("+++login ProviderKeeper", loginResult)
-        //     break
-        //   case EAuthType.ProviderCloud:
-        //     break
-        //   case EAuthType.ProviderWeb:
-        //     break
-        //   case EAuthType.ProviderLedger:
-        //     break
-        //   case EAuthType.ProviderMetamask:
-        //     break
-        //   case EAuthType.ProviderSeed:
-        //     break
-        // }
-
-
+        let provider = null
+        switch (providerType) {
+          case EAuthProvider.keeper:
+            provider = new ProviderKeeper();
+            break
+          case EAuthProvider.ledger:
+            provider = new ProviderLedger();
+            break
+          case EAuthProvider.metamask:
+            provider = new ProviderMetamask()
+            break
+          case EAuthProvider.cloud:
+            provider = new ProviderCloud()
+            break
+          case EAuthProvider.web:
+            console.log("+++web")
+            provider = new ProviderWeb()
+            break
+          case EAuthProvider.seed:
+            provider = new ProviderSeed()
+            break
+        }
+        this.signer.setProvider(provider);
+        return this.signer.login()
       }),
+      catchError((error) => {
+        console.log("+++error", error)
+        return of(null)
+      })
     )
-    .subscribe(event => {
-    console.log("+++subscribe event", event)
+    .subscribe((data: IUser | null) => {
+
+        console.log("+++subscribe event", event)
+        // if (data?.address) {
+        //   this.user$.next(data)
+        // }
+        //
     })
 
 
@@ -94,31 +108,13 @@ export class SignerService {
   }
 
   logout = () => {
-    this.event$.next({type: 'logout'})
+    console.log("!!!")
+    this.event$.next({type: 'logout', timestamp: Date.now()})
   }
 
-  async login(providerType: EAuthProvider) {
-
-    this.event$.next({type: 'login', provider: providerType})
-
-    // switch (authType) {
-    //   case EAuthType.ProviderKeeper:
-    //     const keeper = new ProviderKeeper();
-    //     this.signer.setProvider(keeper);
-    //     const loginResult = await this.signer.login()
-    //     console.log("+++login ProviderKeeper", loginResult)
-    //     break
-    //   case EAuthType.ProviderCloud:
-    //     break
-    //   case EAuthType.ProviderWeb:
-    //     break
-    //   case EAuthType.ProviderLedger:
-    //     break
-    //   case EAuthType.ProviderMetamask:
-    //     break
-    //   case EAuthType.ProviderSeed:
-    //     break
-    // }
+  async login(providerType: EAuthProvider, seed: string | null = null) {
+    console.log("!!!2")
+    this.event$.next({type: 'login', provider: providerType, seed, timestamp: Date.now()})
   }
 
   getBalance = async () => {
